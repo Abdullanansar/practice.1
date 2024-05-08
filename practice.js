3b69b0e5eadef5330fbc081938b12d3ef47fc1da
practice.1 <abdullahansar201@gmail.com>
2024-05-07 22:30:34 -0700

    deploying

---
+++ practice.js
@@ -0,0 +1,25 @@
+
+
+var a = 1;
+document.write("<ul>");
+while (a <= 10){
+    document.write("<li>" + a + "</li>");
+    a++;
+  
+}
+  document.write("</ul>")
+
+
+
+
+
+
+
+
+var a = 1;
+do{
+    document.write(a  + " ) hello <br> ");
+    a++;
+
+  }while (a <= 10)
+
